6e22255527e79c0dfc0f8014e06421ac0ec315be
kacpereqo <remzak.k@gmail.com>
2024-03-22 23:42:19 +0100

    asd

--- backend/scrapper/src/queue/queueTask.py
+++ backend/scrapper/src/queue/queueTask.py
@@ -15,4 +15,4 @@ def queue_task(offertProvider: OffertProvider) -> None:
             dto=dto,
             offertProvider=offertProvider,
         )
-        return pipe(pipeData, (), distanceService.pipe, sexsexasd)
+        return pipe(pipeData, (), distanceService.pipe, sexsexaasdasdsd)
